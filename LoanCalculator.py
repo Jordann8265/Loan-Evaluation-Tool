223a65bc5afcaf2053c31c8578565f06270b712f
Kenn <jkennedy2897@gmail.com>
2026-02-07 22:42:41 -0500

    in-process

--- LoanCalculator.py
+++ LoanCalculator.py
@@ -7,3 +7,10 @@ monthly_payment = float(input("Please enter monthly payment amount: "))
 DAYS_IN_YEAR = 365
 DAYS_IN_MONTH = 30.42 # (Standard number used in these daily balance method interest calculations)
 daily_rate = annual_rate / DAYS_IN_YEAR # Rate of daily interest accrual for daily balance method
+
+balance = principal
+payment_number = 0
+
+# Print the header
+print("\nPmt # | Balance      | Interest  | Principal")
+print("-" * 45)
